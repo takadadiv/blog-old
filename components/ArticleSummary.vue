d36e485d12ecdc7f426b4faea14faef8ef42d445
takadadiv <takadadiv@gmail.com>
2019-12-11 12:16:37 +0900

    記事概要をコンポーネント化

---
+++ components/ArticleSummary.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+    .uk-grid-collapse(uk-grid)
+      .uk-width-expand
+        .summary
+          .date
+            time {{ article.sys.createdAt }}
+          .title
+            n-link(:to="{ name: 'articles-id', params: { id: article.sys.id } }") {{ article.fields.title }}
+          .tag
+            n-link(
+              v-for="tag in article.fields.tags"
+              :key="tag.sys.id"
+              :to="{ name: 'tags-id', params: { id: tag.sys.id } }"
+            ) {{ tag.fields.name }}
+</template>
+
+<script>
+export default {
+  props: {
+    article: Object
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.summary
+  padding 30px
+  .title
+    margin-top 5px
+    font-size 20px
+  .tag
+    margin-top 10px
+</style>
--- pages/tags/_id.vue
+++ pages/tags/_id.vue
@@ -1,24 +1,17 @@
 <template lang="pug">
-div
-  .uk-grid-collapse(uk-grid v-for="article in articles" :key="article.sys.id")
-    .uk-width-expand
-      .summary
-        .date
-          time {{ article.sys.createdAt }}
-        .title
-          n-link(:to="{ name: 'articles-id', params: { id: article.sys.id } }") {{ article.fields.title }}
-        .tag
-          n-link(
-            v-for="tag in article.fields.tags"
-            :key="tag.sys.id"
-            :to="{ name: 'tags-id', params: { id: tag.sys.id } }"
-          ) {{ tag.fields.name }}
+  div
+    ArticleSummary(v-for="article in articles" :key="article.sys.id" :article="article")
 </template>
 
 <script>
 import client from '~/plugins/contentful'
+import ArticleSummary from '~/components/ArticleSummary'
 
 export default {
+  components: {
+    ArticleSummary
+  },
+
   async asyncData({ params }) {
     const res = await client.getEntries({
       content_type: 'article',
